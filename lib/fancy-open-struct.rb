d2eb5720957546c4e8d1caff5d2f028ecfabcfb1
Tom Chapin <tchapin@gmail.com>
2014-05-27 15:56:46 -0600

    changing fancy_open_struct to fancy-open-struct everywhere

--- lib/fancy-open-struct.rb
+++ lib/fancy-open-struct.rb
@@ -2,7 +2,7 @@ require 'ostruct'
 require 'forwardable'
 
 class FancyOpenStruct < OpenStruct
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
 
   extend Forwardable
 
--- spec/fancy_open_struct_spec.rb
+++ spec/fancy_open_struct_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-require 'fancy_open_struct'
+require 'fancy-open-struct'
 
 describe FancyOpenStruct do
 
