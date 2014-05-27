b3deaf9a71389676a109160ac984dc071bfeac1c
Tom Chapin <tchapin@gmail.com>
2014-05-27 16:13:34 -0600

    removing unnecessary code climate stuff and bumping version

--- lib/fancy-open-struct.rb
+++ lib/fancy-open-struct.rb
@@ -2,7 +2,7 @@ require 'ostruct'
 require 'forwardable'
 
 class FancyOpenStruct < OpenStruct
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 
   extend Forwardable
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 require 'rspec'
 require 'pry'
 require 'simplecov'
